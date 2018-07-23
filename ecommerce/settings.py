7c7ac1603e58c4f99c751ca62798f2dff7181dc3
João Pedro E. de Julio <joao_p_julio@hotmail.com>
2018-07-23 16:23:02 -0300

    consertando bus

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = '/static/'
 
 
 #Update database configuration with $DATABASE_URL.
-db_from_env - dj_database_url.config(conn_max_age = 500)
+db_from_env = dj_database_url.config(conn_max_age = 500)
 DATABASES['default'].update(db_from_env)
 
 
