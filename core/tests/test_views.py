3e2c0569a9687902e91453bb7a187c5f52b8f506
João Pedro E. de Julio <joao_p_julio@hotmail.com>
2018-07-23 15:10:01 -0300

    seila

---
+++ core/tests/test_views.py
@@ -0,0 +1,24 @@
+
+
+
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class IndexViewTestCase(TestCase):
+
+	def setUp(self): #cria
+		self.client = Client()
+		self.url = reverse('index')
+
+
+	def tearDown(self): #qndo acaba o teste. remove
+		pass	
+
+	def test_status_code(self):
+		response = self.client.get(self.url)
+		self.assertEquals(response.status_code, 200)
+
+	def test_template_used(self):
+		response = self.client.get(self.url)
+		self.assertTemplateUsed(response, 'index.html')
---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	#texts = ['batata123', 'vamo ve oq rola']
+
+	#context = {
+	#	'title':'éoq? ',
+	#	'texts': texts
+
+	#}
+	return render(request, 'index.html') #quando usar a parte comentada colocar isso dps do index.html: , context
+
+
+def contact(request):
+	return render(request, 'contact.html')
+
+def products(request):
+	return render(request, 'products.html')
+
+def product_list(request):
+	return render(request, 'product_list.html')			
